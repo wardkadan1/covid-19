53554f572a61194d79a6db980c3e1fed209d1c1d
aliab9 <73487656+aliab9@users.noreply.github.com>
2021-01-10 01:40:30 +0200

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,40 @@
+from tkinter.ttk import *
+from tkinter import*
+import tkinter.messagebox
+import sqlite3
+import socket,select
+#from ali import*
+
+def new_win():
+    root = Tk()
+    root.geometry("250x300")
+
+
+port = 12345
+socket_list = []
+users = {}
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('',port))
+server_socket.listen(5)
+socket_list.append(server_socket)
+while True:
+    ready_to_read,ready_to_write,in_error = select.select(socket_list,[],[],0)
+    for sock in ready_to_read:
+        if sock == server_socket:
+            connect, addr = server_socket.accept()
+            socket_list.append(connect)
+            connect.send("You are connected from:" + str(addr))
+        else:
+            try:
+                data = sock.recv(2048)
+                if data.startswith("#"):
+                    users[data[1:].lower()]=connect
+                    print ("User " + data[1:] +" added.")
+                    connect.send("Your user detail saved as : "+str(data[1:]))
+                elif data.startswith("@"):
+                    users[data[1:data.index(':')].lower()].send(data[data.index(':')+1:])
+            except:
+                continue
+server_socket.close()
+window.mainloop()
